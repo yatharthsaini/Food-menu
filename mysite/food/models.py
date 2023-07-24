ee93d5d5cd32a60b6be6d49efcb30270e4ba7ea1
yatharthsaini <sainiyatharth125@gmail.com>
2023-07-25 00:38:26 +0530

    Created superuser and registered models in admin.py

--- mysite/food/models.py
+++ mysite/food/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+class Item(models.Model):
+
+    # explicitly defining the string representation of this model
+    def __str__(self):
+        return self.item_name
+
+    item_name = models.CharField(max_length=200)
+    item_desc = models.CharField(max_length=200)
+    item_price = models.IntegerField()
+
