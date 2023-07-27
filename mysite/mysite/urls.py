e54affb3d5be655d2297bdfb27e7c831c59b2fe1
yatharthsaini <sainiyatharth125@gmail.com>
2023-07-27 21:50:05 +0530

    Successfully created the login page

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -16,9 +16,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.contrib.auth import views as authentication_views
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("food/", include("food.urls")),
     path("register/", include("users.urls")),
+    path("login/", authentication_views.LoginView.as_view(template_name='users/login.html'), name="login"),
+    path("logout/", authentication_views.LogoutView.as_view(template_name='users/logout.html'), name="logout"),
 ]
--- mysite/users/views.py
+++ mysite/users/views.py
@@ -1,18 +1,19 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth.forms import UserCreationForm
+from .forms import RegisterForm
 from django.contrib import messages
 # Create your views here.
 
 def register(request):
     if request.method == "POST":
-        form = UserCreationForm(request.POST)
+        form = RegisterForm(request.POST)
         # checking the validity of data in form
         if form.is_valid():
+            form.save()
             username = form.cleaned_data.get("username")
             messages.success(request, f'Welcome {username}, your account is created')
             return redirect('food:index')
     else:
-        form = UserCreationForm()
+        form = RegisterForm()
     return render(request, "users/register.html", {'form': form})
 
 
