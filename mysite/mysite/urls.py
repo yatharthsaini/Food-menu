bebb68e045c04d6283d7b76d4646b1b6ff946cb3
yatharthsaini <sainiyatharth125@gmail.com>
2023-07-27 21:13:22 +0530

    Created the user creation form inbuilt in django.contrib.auth.forms

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -20,4 +20,5 @@ from django.urls import path, include
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("food/", include("food.urls")),
+    path("register/", include("users.urls")),
 ]
