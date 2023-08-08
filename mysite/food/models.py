16a11a2c7953aa9825fa8e7073253f4bd4ceaf08
yatharthsaini <sainiyatharth125@gmail.com>
2023-08-08 23:37:13 +0530

    Connected the Item model with the user model in the food.models.py

--- mysite/food/models.py
+++ mysite/food/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+from django.contrib.auth.models import User
+
 
 # Create your models here.
 class Item(models.Model):
@@ -6,8 +8,8 @@ class Item(models.Model):
     # explicitly defining the string representation of this model
     def __str__(self):
         return self.item_name
-
+    user_name = models.ForeignKey(User, on_delete=models.CASCADE, default=1)
     item_name = models.CharField(max_length=200)
     item_desc = models.CharField(max_length=200)
     item_price = models.IntegerField()
-    item_image = models.CharField(max_length=1000, default="https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.everestkitchenpa.com%2Fassets%2Fpages%2FfriedRice.html&psig=AOvVaw3bo40h87K4yQFhxXHtj6UJ&ust=1690480342652000&source=images&cd=vfe&opi=89978449&ved=0CBEQjRxqFwoTCMCwnJX4rIADFQAAAAAdAAAAABAD")
+    item_image = models.CharField(max_length=200, default='https://i0.wp.com/servedcatering.com/wp-content/uploads/2021/05/menu-item-placeholder.png?ssl=1')
